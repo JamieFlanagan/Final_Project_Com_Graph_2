a18da5921aacb8ed31cb06572bd7b4dfc445deaf
JamieFlanagan <jamieflana0@gmail.com>
2024-12-28 14:06:53 +0000

    Flipped the flag image

--- lab2/components/Flag/Flag.h
+++ lab2/components/Flag/Flag.h
@@ -26,10 +26,10 @@ private:
     };
 
     GLfloat uvCoords[12] = {
-        0.0f, 1.0f, // Top-left
-        1.0f, 1.0f, // Top-right
-        0.0f, 0.0f, // Bottom-left
-        1.0f, 0.0f  // Bottom-right
+        1.0f, 0.0f, // Top-left
+        0.0f, 0.0f, // Top-right
+        1.0f, 1.0f, // Bottom-left
+        0.0f, 1.0f  // Bottom-right
     };
 };
 
